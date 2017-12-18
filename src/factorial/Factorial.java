3cab7c3ce2a67f2eac8646eac4b5f53591e62fa6
Gonzalo <Gonzalo@ILoveRedHeads>
2017-12-18 19:58:48 +0100

    Commit para subir final

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -4,7 +4,11 @@ package factorial;
 
 public class Factorial {
 
-
+/**
+ * En la main llamaremos a los metodos mediante un objeto que creamos
+ * y sacamos por pantalla el resultado
+ * @param args 
+ */
     public static void main(String[] args) {
         CalcularFactorial obx = new CalcularFactorial();
         obx.calculoFactorial(obx.numero, obx.factorial);
